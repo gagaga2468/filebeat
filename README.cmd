92b1f960cb9eb37429764c748c47c171853702cc
gagaga2468 <gagaga266668@gmail.com>
2025-12-18 04:25:03 +0800

    add README.cmd

---
+++ README.cmd
@@ -0,0 +1,5 @@
+用途：在CentOS或者Ubuntu上快速部署filebeat，并完成Audit系统审计规则添加，filebeat.yaml配置
+采集常见的system、audit、history、secure、nginx、mysql、docker日志
+
+操作方式：下载shell脚本并放在linux的/tmp目录下，按照提示执行脚本
+wget https://raw.githubusercontent.com/gagaga2468/filebeat/refs/heads/main/install-filebeat-config-input.sh
